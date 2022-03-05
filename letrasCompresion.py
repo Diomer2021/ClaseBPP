f4cc165ec4330c1c03f4fce34432db3a2f8a82d0
Diomer2021 <85360619+Diomer2021@users.noreply.github.com>
2022-03-05 07:27:23 -0500

    Cambios de version de codigo

---
+++ letrasCompresion.py
@@ -0,0 +1,2 @@
+letras = [i for i in 'Python es el mejor lenguaje de programación']
+print(letras)
---
+++ letrasCompresion_v2.py
@@ -0,0 +1,2 @@
+letras = [i for i in 'Python es el mejor lenguaje de programación' if i !='m']
+print(letras)
